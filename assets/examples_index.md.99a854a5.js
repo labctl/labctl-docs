4589d457cb729f292fbac9680f37342b009ff786
kellerza <kellerza@users.noreply.github.com>
2022-08-23 14:42:30 +0000

    deploy: 650dc2bd94c52c543bc8db41df825a9c0627a698

---
+++ assets/examples_index.md.99a854a5.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./app.ec7c4a9b.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"examples/index.md","lastUpdated":1661265713000}'),r={name:"examples/index.md"};function s(n,o,c,d,p,i){return t(),a("div")}var l=e(r,[["render",s]]);export{m as __pageData,l as default};
---
+++ assets/reference_index.md.5974cbb8.js
@@ -0,0 +1,25 @@
+import{_ as s,o as n,c as a,g as e}from"./app.ec7c4a9b.js";const y=JSON.parse('{"title":"Reference","description":"","frontmatter":{},"headers":[{"level":2,"title":"Reference","slug":"reference"}],"relativePath":"reference/index.md","lastUpdated":1661265713000}'),l={name:"reference/index.md"},o=e(`<h2 id="reference" tabindex="-1">Reference <a class="header-anchor" href="#reference" aria-hidden="true">#</a></h2><div class="language-"><span class="copy"></span><pre><code><span class="line"><span style="color:#A6ACCD;">Usage: labctl &lt;command&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">Control your network lab.</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">Flags:</span></span>
+<span class="line"><span style="color:#A6ACCD;">  -h, --help    Show context-sensitive help.</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">Global Flags</span></span>
+<span class="line"><span style="color:#A6ACCD;">  -d, --debug=INT          Enable debug mode.</span></span>
+<span class="line"><span style="color:#A6ACCD;">      --settings=STRING    Settings yaml file.</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">Commands:</span></span>
+<span class="line"><span style="color:#A6ACCD;">  color                  Add some color to any Linux command (i.e. ssh).</span></span>
+<span class="line"><span style="color:#A6ACCD;">  config commit          Commit configuration on the node.</span></span>
+<span class="line"><span style="color:#A6ACCD;">  config compare         Compare rendered templates to the node&#39;s config.</span></span>
+<span class="line"><span style="color:#A6ACCD;">  config send            Send commands to the node (i.e. show)</span></span>
+<span class="line"><span style="color:#A6ACCD;">  config template        Show rendered templates.</span></span>
+<span class="line"><span style="color:#A6ACCD;">  config vars            Show variable inputs.</span></span>
+<span class="line"><span style="color:#A6ACCD;">  serve                  Serve the web UI.</span></span>
+<span class="line"><span style="color:#A6ACCD;">  install-completions    install shell completions</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">Run &quot;labctl &lt;command&gt; --help&quot; for more information on a command.</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">labctl: error: expected one of &quot;color&quot;,  &quot;config&quot;,  &quot;serve&quot;,  &quot;install-completions&quot;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,2),p=[o];function t(c,r,i,C,A,d){return n(),a("div",null,p)}var f=s(l,[["render",t]]);export{y as __pageData,f as default};
